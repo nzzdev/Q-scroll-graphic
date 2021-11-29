cdddc178de0d14badc2c3a288645e6d3c662450a
Michel Grautstueck <michel.grautstueck@nzz.ch>
2021-11-29 12:35:30 +0100

    Add fixture with different aspect ratios

--- routes/fixtures/data.js
+++ routes/fixtures/data.js
@@ -4,6 +4,7 @@ const fixtureDataDirectory = "../../resources/fixtures/data";
 // has to be in sync with files created in build task - see ../../tasks/build.js
 const fixtureData = [
   require(`${fixtureDataDirectory}/two-variants-highlighted-text.json`),
+  require(`${fixtureDataDirectory}/single-variant-different-aspect-ratios.json`),
 ];
 
 module.exports = {
